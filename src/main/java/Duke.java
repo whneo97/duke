04d9c11f2c9bd4d2ee7a3848a2475aed9a3c138a
Neo Weihong <whneo97@hotmail.com>
2019-08-21 21:57:33 +0800

    LEVEL 1: ECHO, EXIT

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -9,6 +11,18 @@ public class Duke {
 
         System.out.println(logo + "\nHello! I'm Duke\n"
         + "What can I do for you?");
-        
+
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String command = sc.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(command);
+            }
+        }
+
     }
 }
