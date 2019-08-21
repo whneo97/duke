3ad90caa11e340dfd55097ee1afcc97811bfad20
Neo Weihong <whneo97@hotmail.com>
2019-08-22 02:31:05 +0800

    LEVEL 5: EXCEPTIONS

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,25 +17,49 @@ public class Duke {
         Scanner sc = new Scanner(System.in);
 
         while (true) {
-            String command = sc.next();
+            try {
 
-            if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            } else if (command.equals("done")) {
-                int n = Integer.parseInt(sc.next());
-                Task.getTaskList().get(n - 1).setDone(true);
-            } else if (command.equals("list")) {
-                Task.printTaskList();
-            } else {
-                String taskString = sc.nextLine().trim();
-                if (command.equals("todo")) {
-                    Todo todo = new Todo(taskString);
-                } else if (command.equals("deadline")) {
-                    Deadline deadline = new Deadline(taskString);
-                } else if (command.equals("event")) {
-                    Event event = new Event(taskString);
+                String command = sc.next();
+
+                if (command.equals("bye")) {
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
+                } else if (command.equals("done")) {
+                    int n = Integer.parseInt(sc.next());
+                    Task.getTaskList().get(n - 1).setDone(true);
+                } else if (command.equals("list")) {
+                    Task.printTaskList();
+                } else {
+                    String taskString = sc.nextLine().trim();
+                    if (command.equals("todo")) {
+                        if (taskString.trim().equals("")) {
+                            throw new DukeException("The description of a todo cannot be empty.");
+                        }
+                        Todo todo = new Todo(taskString);
+                    } else if (command.equals("deadline")) {
+                        if (taskString.trim().equals("")) {
+                            throw new DukeException("The description of a deadline cannot be empty.");
+                        } else if (!taskString.contains(" /by ")) {
+                            throw new DukeException("Please separate deadline description and date/time by \" /by \"");
+                        }
+                        Deadline deadline = new Deadline(taskString);
+                    } else if (command.equals("event")) {
+                        if (taskString.trim().equals("")) {
+                            throw new DukeException("The description of an event cannot be empty.");
+                        } else if (!taskString.contains(" /at ")) {
+                            throw new DukeException("Please separate event description and date/time by \" /at \"");
+                        }
+                        Event event = new Event(taskString);
+                    } else {
+                        if (command.equals("")) {
+                            throw new DukeException("Command cannot be empty.");
+                        } else {
+                            throw new DukeException("I'm sorry, but I don't know what that means :-(");
+                        }
+                    }
                 }
+            } catch (DukeException dukeExc) {
+                System.out.println("OOPS!!! " + dukeExc.getMessage());
             }
         }
 
--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -1,2 +1,9 @@
-public class DukeException {
+class DukeException extends Exception{
+    DukeException(String message) {
+        super(message);
+    }
+
+    DukeException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
