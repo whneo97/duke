4dd054292170b39981f88bd9c54108ca54a501ee
Neo Weihong <whneo97@hotmail.com>
2019-08-21 21:48:45 +0800

    Edit Hello Message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,9 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+
+        System.out.println(logo + "\nHello! I'm Duke\n"
+        + "What can I do for you?");
     }
 }
