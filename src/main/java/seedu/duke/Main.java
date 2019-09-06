a3f61196df7c1e529d8e5db2ef33c2b9c6968222
Neo Weihong <whneo97@hotmail.com>
2019-09-07 05:37:35 +0800

    Add java background picture and aesthetics for scrollbar using css.

--- src/main/java/seedu/duke/Main.java
+++ src/main/java/seedu/duke/Main.java
@@ -21,6 +21,7 @@ public class Main extends Application {
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
+            scene.getStylesheets().addAll(this.getClass().getResource("/view/style.css").toExternalForm());
             stage.setScene(scene);
             fxmlLoader.<MainWindow>getController().setDuke(duke);
             stage.show();
